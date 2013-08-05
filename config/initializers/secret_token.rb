311a1cd590680c34ebd4d4d817be7f3947d084d5
Chris Lee <finges@gmail.com>
2013-08-05 19:59:11 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dotastic::Application.config.secret_key_base = '51fddc81fe6572fec066b81ae224ba7a58e8fcf9668618b293f5db72d6d16aeed289b576fb7fa4954bbe36a4ad54e021fe7d968137ad2bba7eed135a7804e282'
